498139f1f61d633fad972a9b5751037451c30322
cfc603 <wtrevor162@gmail.com>
2017-08-26 16:39:57 -0400

    set PUBLIC_PATH & SHARED_PATH

--- private/initialize.php
+++ private/initialize.php
@@ -2,6 +2,8 @@
 
     define("PRIVATE_PATH", dirname(__FILE__));
     define("PROJECT_PATH", dirname(PRIVATE_PATH));
+    define("PUBLIC_PATH", PROJECT_PATH . "/public");
+    define("SHARED_PATH", PRIVATE_PATH . "/shared");
 
     define("WWW_ROOT", "");
 
