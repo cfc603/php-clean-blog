9fa45648541569c137822a1593cd3c4b8eafa107
cfc603 <wtrevor162@gmail.com>
2017-08-26 16:39:32 -0400

    add private/shared dir and extract reusable code

---
+++ private/shared/header.php
@@ -0,0 +1,13 @@
+<!-- Page Header -->
+<header class="masthead" style="background-image: url('<?php echo url_for($header_img_path); ?>')">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-10 mx-auto">
+        <div class="site-heading">
+          <h1><?php echo $heading ?></h1>
+          <span class="subheading"><?php echo $subheading ?></span>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
